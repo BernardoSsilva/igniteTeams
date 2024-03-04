47af58aed2eb157cc3042a7b32e02fa3b147834f
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-04 14:43:12 -0300

    created theme sheet to project

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,10 @@
 import { View, StatusBar, Text } from "react-native";
-import { Container, MainTitle } from "./styles";
+import * as S from "./styles";
 
 export function Groups() {
   return (
-    <Container>
-      <MainTitle>Hello</MainTitle>
-    </Container>
+    <S.Container>
+      <S.MainTitle>Hello</S.MainTitle>
+    </S.Container>
   );
 }
