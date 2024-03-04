ac35c18fa76b97778caf8cb1df7feac6ec3fcba8
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-04 16:07:19 -0300

    created group card setup

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -2,12 +2,14 @@ import { View, StatusBar, Text } from "react-native";
 import * as S from "./styles";
 import { Header } from "@components/headder";
 import { Highlight } from "@components/highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <S.Container>
       <Header></Header>
-      <Highlight title="Turmas" subtitle="jogue com a sua turma"/>
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <GroupCard title="Test"></GroupCard>
     </S.Container>
   );
 }
