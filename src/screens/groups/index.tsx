2ff820ea85a56f4f30e65d489e334d86136869c1
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-04 15:31:20 -0300

    created headder component

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,9 +1,11 @@
 import { View, StatusBar, Text } from "react-native";
 import * as S from "./styles";
+import { Header } from "@components/headder";
 
 export function Groups() {
   return (
     <S.Container>
+      <Header></Header>
       <S.MainTitle>Hello</S.MainTitle>
     </S.Container>
   );
